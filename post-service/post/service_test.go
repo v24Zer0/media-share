9acd7bfa6df238edf7e6ccf50024fd637cce90bc
v24Zer0 <vinbesse@gmail.com>
2022-09-20 10:15:23 +0200

    Added mock implementations to service_test

--- post-service/post/service_test.go
+++ post-service/post/service_test.go
@@ -2,16 +2,51 @@ package post_test
 
 import (
 	"testing"
+
+	"github.com/v24Zer0/media-share/post-service/mock"
+	"github.com/v24Zer0/media-share/post-service/post"
 )
 
 func TestServiceGetPosts(t *testing.T) {
+	repo := NewMockRepo()
+	idProvider := mock.MockIDProvider{}
 
+	service := post.NewPostService(repo, idProvider)
+
+	service.GetPosts()
 }
 
 func TestServiceCreatePost(t *testing.T) {
+	repo := mock.NewMockRepo()
+	idProvider := mock.MockIDProvider{}
 
+	service := post.NewPostService(repo, idProvider)
+	post := post.Post{
+		ID:        "",
+		Title:     "Title 6",
+		CreatedAt: "",
+		UserID:    "0002",
+		CreatedBy: "user2",
+	}
+
+	// Should create post and return nil err
+	err := service.CreatePost(&post)
+	if err != nil {
+		t.Fatalf("Failed to create post with id: %s", post.ID)
+	}
+
+	// Should fail to create post and return err with message "missing field"
 }
 
 func TestServiceDeletePost(t *testing.T) {
+	repo := mock.NewMockRepo()
+	idProvider := mock.MockIDProvider{}
 
+	service := post.NewPostService(repo, idProvider)
+	id := "1"
+
+	err := service.DeletePost(id)
+	if err != nil {
+		t.Fatalf("Failed to delete post with id: %s", id)
+	}
 }
