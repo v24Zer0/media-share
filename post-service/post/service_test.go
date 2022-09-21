73728fd5ece0f1ae4d1535f93a540d1abaa63359
v24Zer0 <vinbesse@gmail.com>
2022-09-21 09:36:19 +0200

    Implemented PostService tests

--- post-service/post/service_test.go
+++ post-service/post/service_test.go
@@ -8,12 +8,22 @@ import (
 )
 
 func TestServiceGetPosts(t *testing.T) {
-	repo := NewMockRepo()
+	repo := mock.NewMockRepo()
 	idProvider := mock.MockIDProvider{}
 
 	service := post.NewPostService(repo, idProvider)
 
-	service.GetPosts()
+	userID := "0001"
+	posts, _ := service.GetPosts(userID)
+	if len(*posts) != 3 {
+		t.Fatalf("Posts for user(%s) not returned", userID)
+	}
+
+	userID = "0002"
+	posts, _ = service.GetPosts(userID)
+	if len(*posts) != 2 {
+		t.Fatalf("Posts for user(%s) not returned", userID)
+	}
 }
 
 func TestServiceCreatePost(t *testing.T) {
@@ -21,7 +31,7 @@ func TestServiceCreatePost(t *testing.T) {
 	idProvider := mock.MockIDProvider{}
 
 	service := post.NewPostService(repo, idProvider)
-	post := post.Post{
+	post1 := post.Post{
 		ID:        "",
 		Title:     "Title 6",
 		CreatedAt: "",
@@ -30,12 +40,38 @@ func TestServiceCreatePost(t *testing.T) {
 	}
 
 	// Should create post and return nil err
-	err := service.CreatePost(&post)
+	err := service.CreatePost(&post1)
 	if err != nil {
-		t.Fatalf("Failed to create post with id: %s", post.ID)
+		t.Fatalf("Failed to create post with id: %s", post1.ID)
+	}
+
+	post2 := post.Post{
+		ID:        "1",
+		Title:     "Title 6",
+		CreatedAt: "",
+		UserID:    "0002",
+		CreatedBy: "user2",
+	}
+
+	// Should fail to create duplicate post
+	err = service.CreatePost(&post2)
+	if err != nil {
+		t.Fatalf("No error when creating duplicate post with id: %s", post2.ID)
+	}
+
+	post3 := post.Post{
+		ID:        "",
+		Title:     "",
+		CreatedAt: "",
+		UserID:    "0002",
+		CreatedBy: "user2",
 	}
 
 	// Should fail to create post and return err with message "missing field"
+	err = service.CreatePost(&post3)
+	if err == nil {
+		t.Fatal("No error when creating post with missing field")
+	}
 }
 
 func TestServiceDeletePost(t *testing.T) {
@@ -43,10 +79,18 @@ func TestServiceDeletePost(t *testing.T) {
 	idProvider := mock.MockIDProvider{}
 
 	service := post.NewPostService(repo, idProvider)
-	id := "1"
 
+	// should delete post with valid id
+	id := "1"
 	err := service.DeletePost(id)
 	if err != nil {
 		t.Fatalf("Failed to delete post with id: %s", id)
+	}
+
+	// should fail to delete post with unknown id
+	id = "6"
+	err = service.DeletePost(id)
+	if err == nil {
+		t.Fatalf("No error when deleting unknown post with id: %s", id)
 	}
 }
