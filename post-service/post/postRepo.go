f5ee04e3b53de0543227cc977fee7c1c5c17aa3f
v24Zer0 <vinbesse@gmail.com>
2022-09-28 20:02:39 +0200

    Add connection to db in PostRepo

--- post-service/post/postRepo.go
+++ post-service/post/postRepo.go
@@ -1,6 +1,11 @@
 package post
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -8,7 +13,23 @@ type PostRepo struct {
 	db *gorm.DB
 }
 
-func NewPostRepo(db *gorm.DB) *PostRepo {
+func ConstructDBPath() string {
+	dbURL := os.Getenv("DB_URL")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+
+	return fmt.Sprintf("%s:%s@tcp%s/%s", dbUser, dbPass, dbURL, dbName)
+}
+
+func NewPostRepo() *PostRepo {
+	dbPath := ConstructDBPath()
+
+	db, err := gorm.Open(mysql.Open(dbPath))
+	if err != nil {
+		log.Panicln("Failed to connect to database")
+	}
+
 	return &PostRepo{
 		db: db,
 	}
