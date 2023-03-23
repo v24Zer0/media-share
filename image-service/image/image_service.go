0f712583f2549054defbb70708842c6b5efec2be
v24Zer0 <vinbesse@gmail.com>
2023-03-23 13:15:34 +0200

    Added service methods to image service

--- image-service/image/image_service.go
+++ image-service/image/image_service.go
@@ -1,1 +1,43 @@
 package image
+
+import "github.com/v24Zer0/media-share/image-service/util"
+
+type ImageService struct {
+	repo       Repo
+	idProvider util.IDProvider
+}
+
+func NewImageService(repo Repo, idProvider util.IDProvider) *ImageService {
+	return &ImageService{
+		repo:       repo,
+		idProvider: idProvider,
+	}
+}
+
+func (service ImageService) GetImage(postID string) ([]byte, error) {
+	// Return image as bytes written to conttext Writer
+	// b, err := ioutil.ReadFileutil
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// ctx.Writer.Write(b)
+	return []byte{}, nil
+}
+
+func (service ImageService) CreateImage(postID string) error {
+	id := service.idProvider.GenerateID()
+	// path := create new image path
+	image := &Image{
+		ID:     id,
+		Path:   "",
+		PostID: postID,
+	}
+
+	service.repo.CreateImage(image)
+	return nil
+}
+
+func (service ImageService) DeleteImage(postID string) error {
+	return nil
+}
