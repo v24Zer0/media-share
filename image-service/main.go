152f2d1afd3f5ba91a4af2347f32b1d0645c5a5f
v24Zer0 <vinbesse@gmail.com>
2023-03-23 13:14:58 +0200

    Created handler, service and repo in main

--- image-service/main.go
+++ image-service/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/v24Zer0/media-share/image-service/image"
+	"github.com/v24Zer0/media-share/image-service/router"
+	"github.com/v24Zer0/media-share/image-service/util"
 )
 
 func main() {
-	// r := router.NewRouter()
-	fmt.Println("Hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading environment variables")
+	}
+
+	repo := image.NewImageRepo()
+	service := image.NewImageService(repo, util.DefaultProvider{})
+	handler := image.NewImageHandler(service)
+
+	r := router.NewRouter(handler)
+	r.Run(":8081")
 }
