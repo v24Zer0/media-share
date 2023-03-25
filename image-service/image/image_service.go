c216dcc192288807c3b041e308b93d118dfb45a3
v24Zer0 <vinbesse@gmail.com>
2023-03-25 17:40:52 +0200

    Add file to service method to create image

--- image-service/image/image_service.go
+++ image-service/image/image_service.go
@@ -1,6 +1,10 @@
 package image
 
-import "github.com/v24Zer0/media-share/image-service/util"
+import (
+	"mime/multipart"
+
+	"github.com/v24Zer0/media-share/image-service/util"
+)
 
 type ImageService struct {
 	repo       Repo
@@ -25,14 +29,19 @@ func (service ImageService) GetImage(postID string) ([]byte, error) {
 	return []byte{}, nil
 }
 
-func (service ImageService) CreateImage(postID string) error {
+func (service ImageService) CreateImage(postID string, file multipart.File) error {
 	id := service.idProvider.GenerateID()
 	// path := create new image path
+
 	image := &Image{
 		ID:     id,
 		Path:   "",
 		PostID: postID,
 	}
+
+	// fileBytes := []byte{}
+	// file.Read(fileBytes)
+	// defer file.Close()
 
 	service.repo.CreateImage(image)
 	return nil
--- image-service/image/service.go
+++ image-service/image/service.go
@@ -1,7 +1,9 @@
 package image
 
+import "mime/multipart"
+
 type Service interface {
 	GetImage(postID string) ([]byte, error)
-	CreateImage(postID string) error
+	CreateImage(postID string, file multipart.File) error
 	DeleteImage(postID string) error
 }
