164632d0c4dfb2bc5ddf268406ee9b6d232ff92b
v24Zer0 <vinbesse@gmail.com>
2023-04-04 16:04:10 +0200

    Removed GenerateFileID in IDProvider

--- image-service/util/id_provider.go
+++ image-service/util/id_provider.go
@@ -2,16 +2,11 @@ package util
 
 type IDProvider interface {
 	GenerateID() string
-	GenerateFileID() string
 }
 
 type DefaultProvider struct {
 }
 
 func (provider DefaultProvider) GenerateID() string {
-	return ""
-}
-
-func (provider DefaultProvider) GenerateFileID() string {
 	return ""
 }
