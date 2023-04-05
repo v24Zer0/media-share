ea75a63d157178e2827477a9d2322c119db15c88
v24Zer0 <vinbesse@gmail.com>
2023-04-05 13:51:20 +0200

    Added uuid for image id generation

--- image-service/go.mod
+++ image-service/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.9.0
+	github.com/google/uuid v1.3.0
 	gorm.io/gorm v1.24.6
 )
 
--- image-service/util/id_provider.go
+++ image-service/util/id_provider.go
@@ -1,5 +1,7 @@
 package util
 
+import "github.com/google/uuid"
+
 type IDProvider interface {
 	GenerateID() string
 }
@@ -8,5 +10,9 @@ type DefaultProvider struct {
 }
 
 func (provider DefaultProvider) GenerateID() string {
-	return ""
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return ""
+	}
+	return id.String()
 }
