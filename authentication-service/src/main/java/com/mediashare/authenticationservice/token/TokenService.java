d2e93afed70dd7b1a9d4661a613f1785db49a4c1
v24Zer0 <vinbesse@gmail.com>
2023-04-07 11:57:06 +0200

    Added placeholder route

--- authentication-service/src/main/java/com/mediashare/authenticationservice/token/TokenService.java
+++ authentication-service/src/main/java/com/mediashare/authenticationservice/token/TokenService.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenService {
 
-
+    public TokenResponse getToken() {
+        return new TokenResponse("token", "user");
+    }
 }
