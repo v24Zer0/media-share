445d552897a4ff79395508e4a7bf8dbafdaac5d3
v24Zer0 <vinbesse@gmail.com>
2023-04-15 18:21:35 +0200

    Implemented uuid validation

--- authentication-service/src/main/java/com/mediashare/authenticationservice/util/UUIDRegex.java
+++ authentication-service/src/main/java/com/mediashare/authenticationservice/util/UUIDRegex.java
@@ -1,8 +1,15 @@
 package com.mediashare.authenticationservice.util;
 
+import java.util.regex.Pattern;
+
 public class UUIDRegex {
+    private final Pattern pattern;
+
+    public UUIDRegex() {
+        pattern = Pattern.compile("^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$");
+    }
 
     public boolean validateID(String id) {
-        return true;
+        return this.pattern.matcher(id).matches();
     }
 }
