debead10b0114bfc16faf42d8767720dd85244d2
v24Zer0 <vinbesse@gmail.com>
2023-04-15 11:06:10 +0200

    Added TokenService test functions

--- authentication-service/src/test/java/com/mediashare/authenticationservice/TokenServiceTests.java
+++ authentication-service/src/test/java/com/mediashare/authenticationservice/TokenServiceTests.java
@@ -1,15 +1,47 @@
 package com.mediashare.authenticationservice;
 
+import com.mediashare.authenticationservice.token.DefaultTokenService;
+import com.mediashare.authenticationservice.token.TokenService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TokenServiceTests {
+    private TokenService tokenService;
+    @BeforeEach
+    public void setup() {
+        this.tokenService = new DefaultTokenService();
+    }
 
     @Test
-    @DisplayName("Should match given string")
-    public void testMatchString() {
-        String hello = "hello";
-        assertThat(hello).isEqualTo("hello");
+    @DisplayName("Should successfully create token with user claim matching given userID")
+    public void testCreateToken() {
+        String token = this.tokenService.createToken("user_01");
+        assertThat(token).isNotEqualTo("");
+    }
+
+    @Test
+    @DisplayName("Should fail to create token with JWTException thrown")
+    public void testCreateTokenWithJWTCreationException() {
+
+    }
+
+    @Test
+    @DisplayName("Should successfully verify token")
+    public void testVerifyToken() {
+
+    }
+
+    @Test
+    @DisplayName("Should fail to verify token with JWTVerificationException")
+    public void testVerifyTokenWithJWTVerificationException() {
+
+    }
+
+    @Test
+    @DisplayName("Should fail to verify token because of missing user claim")
+    public void testVerifyTokenWithMissingUserClaim() {
+
     }
 }
