eda3286bd8c4365cdec7f9383d8ff5b7743cc873
v24Zer0 <vinbesse@gmail.com>
2023-04-15 11:19:19 +0200

    TokenService tests

--- authentication-service/src/test/java/com/mediashare/authenticationservice/TokenServiceTests.java
+++ authentication-service/src/test/java/com/mediashare/authenticationservice/TokenServiceTests.java
@@ -1,5 +1,7 @@
 package com.mediashare.authenticationservice;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.mediashare.authenticationservice.token.DefaultTokenService;
 import com.mediashare.authenticationservice.token.TokenService;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +21,7 @@ public class TokenServiceTests {
     public void testCreateToken() {
         String token = this.tokenService.createToken("user_01");
         assertThat(token).isNotEqualTo("");
+//        check that returned token is a valid jwt
     }
 
     @Test
@@ -30,18 +33,40 @@ public class TokenServiceTests {
     @Test
     @DisplayName("Should successfully verify token")
     public void testVerifyToken() {
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        String testToken = JWT.create().withClaim("user", "user_01").sign(algorithm);
 
+        boolean ok = this.tokenService.verifyToken(testToken);
+        assertThat(ok).isEqualTo(true);
     }
 
     @Test
     @DisplayName("Should fail to verify token with JWTVerificationException")
     public void testVerifyTokenWithJWTVerificationException() {
+        Algorithm algorithm = Algorithm.HMAC256("invalid_secret");
+        String testToken = JWT.create().withClaim("user", "user_01").sign(algorithm);
 
+        boolean ok = this.tokenService.verifyToken(testToken);
+        assertThat(ok).isEqualTo(false);
     }
 
     @Test
     @DisplayName("Should fail to verify token because of missing user claim")
     public void testVerifyTokenWithMissingUserClaim() {
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        String testToken = JWT.create().sign(algorithm);
 
+        boolean ok = this.tokenService.verifyToken(testToken);
+        assertThat(ok).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Should fail to verify token because of missing user claim")
+    public void testVerifyTokenWithEmptyUserClaim() {
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        String testToken = JWT.create().withClaim("user", "").sign(algorithm);
+
+        boolean ok = this.tokenService.verifyToken(testToken);
+        assertThat(ok).isEqualTo(false);
     }
 }
