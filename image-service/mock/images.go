dc45ae0f7cca9309c7c5fd01ec26f08769226c36
v24Zer0 <vinbesse@gmail.com>
2023-04-22 18:21:43 +0200

    Added mock images

---
+++ image-service/mock/images.go
@@ -0,0 +1,21 @@
+package mock
+
+import "github.com/v24Zer0/media-share/image-service/image"
+
+var images = []image.Image{
+	{
+		ID:     "700508e7-b810-496e-b7e3-d8d1c6fcf83e",
+		Path:   "/upload/images/700508e7-b810-496e-b7e3-d8d1c6fcf83e.jpg",
+		PostID: "post_01",
+	},
+	{
+		ID:     "547da3f6-a50a-43db-b928-e9a52f726a66",
+		Path:   "/upload/images/547da3f6-a50a-43db-b928-e9a52f726a66.jpg",
+		PostID: "post_02",
+	},
+	{
+		ID:     "02f0d331-3ed1-43cc-8f88-0eb4b9501d8c",
+		Path:   "/upload/images/02f0d331-3ed1-43cc-8f88-0eb4b9501d8c.jpeg",
+		PostID: "post_03",
+	},
+}
